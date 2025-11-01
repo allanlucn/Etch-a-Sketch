233d058e4079e130054708043e22099adf4a4a4b
Allan <172357148+allanlucn@users.noreply.github.com>
2025-11-01 09:55:45 -0300

    grid aprimorado

--- draw.js
+++ draw.js
@@ -1,19 +1,20 @@
 const gridContainer = document.querySelector("#grid");
 
-let size = 16;
-gridContainer.style.gridTemplateColumns =`repeat(${size},1fr)`
-gridContainer.style.gridTemplateRows =`repeat(${size},1fr)`
 function createPannels(size){
+    gridContainer.style.gridTemplateColumns =`repeat(${size},1fr)`
+    gridContainer.style.gridTemplateRows =`repeat(${size},1fr)`
     for(let i =0; i<size; i++){
         for(let j=0; j<size; j++){
             const pannel = document.createElement('div');
             gridContainer.appendChild(pannel);
-            pannel.style.width = '100%';
-            pannel.style.height = '100%';
             pannel.style.backgroundColor = 'white';
             pannel.style.border = 'solid 0.1px black'
+            pannel.addEventListener('mouseenter',(event) =>{
+                pannel.style.backgroundColor = 'black';
+            })
         }
     }
 };
 
-createPannels(16);
+
+createPannels(32);
